8f4af4c9ccdcde84d9e031b667e29d7b98122107
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-02-27 22:20:28 -0800

    solve problem duplicate element check

---
+++ ContainsDuplicate/ContainsDuplicate/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 1 };
+        Console.WriteLine(ContainsDuplicate(nums));
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        var memo = new Dictionary<int, int>();
+        foreach (int n in nums)
+        {
+            if (memo.ContainsKey(n))
+                return true;
+            else if (!memo.ContainsKey(n))
+                memo[n] = 1;
+            else
+                memo[n] += 1;
+        }
+        return false;
+    }
+}
