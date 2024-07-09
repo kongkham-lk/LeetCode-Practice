9979264fb8d321b23337f876e78f82507aedc944
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-07-09 16:52:06 -0700

    solved problem 2117

--- Abbreviating the Product of a Range/Abbreviating the Product of a Range/Program.cs
+++ Abbreviating the Product of a Range/Abbreviating the Product of a Range/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Abbreviating_the_Product_of_a_Range;
 
-string result = new Solution().AbbreviateProduct(44, 9915);
+string result = new Solution().AbbreviateProduct_Optimized(1, 6);
 
 Console.WriteLine($"Return string: {result}\n");
 
--- Abbreviating the Product of a Range/Abbreviating the Product of a Range/Solution.cs
+++ Abbreviating the Product of a Range/Abbreviating the Product of a Range/Solution.cs
@@ -7,7 +7,7 @@ namespace Abbreviating_the_Product_of_a_Range
         {
         }
 
-        public string AbbreviateProduct(int left, int right)
+        public string AbbreviateProduct_InitialImplement(int left, int right)
         {
             long lProduct = 1;
             long rProduct = 1;
@@ -52,6 +52,45 @@ namespace Abbreviating_the_Product_of_a_Range
                 result = result.Substring(0, 5) + "..." + result.Substring(result.Length - 5);
 
             return result + "e" + c;
+        }
+
+
+        public string AbbreviateProduct_Optimized(int left, int right)
+        {
+            // need to have 2 variable to solve this problem
+            // since the greater the number, the longer it will take to do the multiplication
+            double pref = 1.0; // limit to 1 digit, Fast but not precise due to Double's floating point rounding error
+            long suff = 1; // for removing zero and limit digits amount 10^14 (the acceptable size of int64)
+
+            int totalDigit = 0;
+            int zeroTrailing = 0;
+
+            for (int i = left; i <= right; i++)
+            {
+                pref *= i;
+                suff *= i;
+                while (pref >= 1.0) // checking the new value's total digit
+                {
+                    pref /= 10.0;
+                    totalDigit++; // keep the number of total digit up-to-date and reduce the actual value to 1 digit
+                }
+                while (suff % 10 == 0) // checking trailing zero
+                {
+                    suff /= 10;
+                    zeroTrailing++; // remove trailing zero while memorize it
+                }
+                if (suff > Math.Pow(10, 14))
+                    suff = suff % (long)Math.Pow(10, 14); // need to convert Math.Pow() to Long first before do the mod opearation, else return wrong value
+            }
+
+            string result = "";
+
+            if (totalDigit - zeroTrailing > 10)
+                result = (int)(pref * Math.Pow(10, 5)) + "..." + suff.ToString().Substring(suff.ToString().Length - 5); // the suff need to use SubString() only, since it somehow fail to display the last five digit when using mod operator.
+            else
+                result = (long)(pref * Math.Pow(10, totalDigit - zeroTrailing) + 0.5) + ""; // need to increment 0.5 due to the imprecision of Doubles that cause by floating point rounding errors.
+
+            return result + "e" + zeroTrailing;
         }
     }
 }
