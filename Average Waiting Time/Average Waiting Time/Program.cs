42c54f0d8d83cf9802d6881a783acf778503924b
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-07-08 19:20:31 -0700

    solve problem 1701

---
+++ Average Waiting Time/Average Waiting Time/Program.cs
@@ -0,0 +1,5 @@
+﻿using Average_Waiting_Time;
+
+int[][] customers = new int[][] { new int[]{ 1, 2 }, new int[] { 2, 5 }, new int[] { 4, 3 } };
+
+Console.WriteLine($"\nAverage waiting time is: {new Solution().AverageWaitingTime(customers)}\n");
---
+++ Average Waiting Time/Average Waiting Time/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Average_Waiting_Time
+{
+	public class Solution
+	{
+		public Solution()
+		{
+		}
+
+        public double AverageWaitingTime(int[][] customers)
+        {
+            double averageWaitingTime = 0;
+            int prevCookTime = 0;
+
+			for (int i = 0; i < customers.Length; i++)
+			{
+                prevCookTime = (prevCookTime > customers[i][0] ? prevCookTime : customers[i][0]) + customers[i][1];
+                averageWaitingTime += prevCookTime - customers[i][0];
+
+                Console.WriteLine($"customer {i + 1} wait time: {averageWaitingTime}");
+            }
+
+            return averageWaitingTime / customers.Length;
+        }
+    }
+}
+
