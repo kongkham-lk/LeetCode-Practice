c5c04294740b55effc9305720d6ddab296d51d5b
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-07-11 00:04:59 -0700

    solve problem 1598

---
+++ Crawler Log Folder/Crawler Log Folder/Program.cs
@@ -0,0 +1,9 @@
+﻿using Crawler_Log_Folder;
+
+string[] logs1 = { "d1/", "d2/", "../", "d21/", "./" };
+string[] logs2 = { "d1/", "d2/", "./", "d3/", "../", "d31/" };
+string[] logs3 = { "d1/", "../", "../", "../" };
+
+Console.WriteLine("logs1's crawling foler: " + new Solution().MinOperations(logs1));
+Console.WriteLine("logs2's crawling foler: " + new Solution().MinOperations(logs2));
+Console.WriteLine("logs3's crawling foler: " + new Solution().MinOperations(logs3));
---
+++ Crawler Log Folder/Crawler Log Folder/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Crawler_Log_Folder
+{
+    public class Solution
+    {
+        public int MinOperations(string[] logs)
+        {
+            int steps = 0;
+            foreach (string log in logs)
+            {
+                if (log.Equals("../"))
+                {
+                    if (steps > 0)
+                        steps--;
+                }
+                else if (!log.Equals("./"))
+                {
+                    steps++;
+                }
+            }
+            return steps;
+        }
+    }
+}
+
