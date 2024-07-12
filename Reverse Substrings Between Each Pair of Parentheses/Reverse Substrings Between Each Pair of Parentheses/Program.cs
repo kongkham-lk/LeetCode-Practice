2a65c31b642299f3c96b4057269d30945b9d576f
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-07-11 17:42:59 -0700

    attempting to solve problem 1190

---
+++ Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses/Program.cs
@@ -0,0 +1,11 @@
+﻿using Reverse_Substrings_Between_Each_Pair_of_Parentheses;
+
+string test1 = "(abcd)";
+string test2 = "(u(love)i)";
+string test3 = "(ed(et(oc))el)";
+string test4 = "a(bcdefghijkl(mno)p)q";
+
+Console.WriteLine($"Input: {test1} \nExpected: dcba \nOutput: {new Solution().ReverseParentheses(test1)}\n");
+Console.WriteLine($"Input: {test2} \nExpected: iloveu \nOutput: {new Solution().ReverseParentheses(test2)}\n");
+Console.WriteLine($"Input: {test3} \nExpected: leetcode \nOutput: {new Solution().ReverseParentheses(test3)}\n");
+Console.WriteLine($"Input: {test4} \nExpected: apmnolkjihgfedcbq \nOutput: {new Solution().ReverseParentheses(test4)}\n");
---
+++ Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Reverse_Substrings_Between_Each_Pair_of_Parentheses;
+
+public class Solution
+{
+    public string ReverseParentheses(string s)
+    {
+        s = ReverseSubString(s);
+        return s;
+    }
+
+    private string ReverseSubString(string s)
+    {
+        // ( ed(et(oc))el )
+        // ed( et(oc) )el
+        // et( oc )
+
+        string pre = "";
+        string mid = "";
+        string suf = "";
+        string result = "";
+        int foundIndexClose = 0;
+
+        int foundIndexOpen = s.IndexOf("(");
+        if (foundIndexOpen != -1)
+        {
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i].ToString().Equals(")"))
+                {
+                    foundIndexClose = i;
+                    break;
+                }
+            }
+
+            pre = s.Substring(0, foundIndexOpen); // ed
+            mid = s.Substring(foundIndexOpen + 1, foundIndexClose - (foundIndexOpen + 1)); // et( oc )
+            suf = s.Substring(foundIndexClose + 1); // el
+            mid = ReverseSubString(mid); // etco
+        }
+
+        foreach (char substr in mid)
+            result = substr + result; // octe
+
+        return pre + result + suf; // edocteel
+    }
+}
+
