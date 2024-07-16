923e7cc92844a7f69af039b46fd2c27ea87a4a65
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-07-15 18:20:24 -0700

    Added documentation

--- Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses/Solution.cs
+++ Reverse Substrings Between Each Pair of Parentheses/Reverse Substrings Between Each Pair of Parentheses/Solution.cs
@@ -13,24 +13,20 @@ public class Solution
 
         foreach (char c in s)
         {
-            if (!c.Equals(')'))
+            if (!c.Equals(')')) // we assume that if there is ')' then there should be a '('
                 stack.Push(c);
             else
             {
-                while (stack.TryPop(out char c1) && !c1.Equals('('))
-                {
-                    Console.WriteLine("c1 is: " + c1);
+                // pop first then check if equal '(', thus not require the extra '(' pop step
+                while (stack.TryPop(out char c1) && !c1.Equals('(')) // pop the '(' as the last one
                     queue.Enqueue(c1);
-                }
-                Console.WriteLine("Start insert back!!!");
-                while (queue.TryDequeue(out char c2))
+                while (queue.TryDequeue(out char c2)) // reverse and store back using queue and stack
                     stack.Push(c2);
-                Console.WriteLine("Done back!!!");
             }
         }
 
         string result = "";
-        while (stack.TryPop(out char r))
+        while (stack.TryPop(out char r)) // pop and save as r and store to result string from the last element.
             result = r + result;
 
         return result;
