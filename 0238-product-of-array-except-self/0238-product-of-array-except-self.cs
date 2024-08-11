75d0c82ef5c90fb9893a1113cd20a13d146c710b
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-08-10 17:23:11 -0700

    Time: 174 ms (76.25%), Space: 65.8 MB (76.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -8,10 +8,10 @@ public class Solution {
             pre = results[i] * nums[i]; // multiply the left with current nums to be the nex element's left
         }
         
-        int pos = nums[nums.Length - 1]; // initilized the right
+        int pos = 1; // initilized the right
         
         // skip last element
-        for (int i = nums.Length - 2; i >= 0; i--) {
+        for (int i = nums.Length - 1; i >= 0; i--) {
             results[i] *= pos; // multiply the right and the left
             pos *= nums[i]; // multiply the right with current to be nex element's right
         }
