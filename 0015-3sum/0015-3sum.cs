1860c8ab97bde3816caac6db57ddfe60363d199c
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-12-15 18:55:15 -0800

    Time: 32 ms (67.16%), Space: 65.3 MB (82.29%) - LeetHub

--- 0015-3sum/0015-3sum.cs
+++ 0015-3sum/0015-3sum.cs
@@ -10,16 +10,21 @@ public class Solution {
 
             int j = i + 1;
             int k = nums.Length - 1;
+            int valI = nums[i];
+            int valIJ = valI + ((j < k) ? nums[j] : 0);
 
             while (j < k)
             {
-                int total = nums[i] + nums[j] + nums[k];
+                int total = valIJ + nums[k];
 
                 // need to k-- instead of j++ because the array is already sorted, if too big then shift down, if too small then shift up
                 if (total > 0)
                     k--;
-                else if (total < 0)
+                else if (total < 0) {
                     j++;
+                    if (j < k)
+                        valIJ = valI + nums[j];
+                }
                 else
                 {
                     resultList.Add(new List<int>() {nums[i], nums[j], nums[k]});
@@ -27,6 +32,8 @@ public class Solution {
                     
                     while (nums[j] == nums[j-1] && j < k)  // skip duplicate el of j
                         j++;
+                    if (j < k)
+                        valIJ = valI + nums[j];
                 }
             }
         }
