6c61ad6a5ced0404cae16f1558eaf5e847692d92
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-12-15 19:46:42 -0800

    Time: 0 ms (100.00%), Space: 27.3 MB (32.39%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cs
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cs
@@ -1,6 +1,10 @@
 public class Solution {
     public int GetSum(int a, int b) {
-        int[] nums = new int[] {a, b};
-        return nums.Sum();
+        while (b != 0) { // b as carry, a as result, no need to use 2's compliment here
+            int tmp = (a & b) << 1;
+            a = a ^ b;
+            b = tmp;
+        }
+        return a;
     }
 }
