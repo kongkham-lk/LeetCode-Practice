a5a36e656e2bf0cd9d8742f08f998c1929ecd717
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-12-16 20:46:54 -0800

    Time: 0 ms (100.00%), Space: 27.6 MB (26.72%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int HammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1)
+                count++;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
