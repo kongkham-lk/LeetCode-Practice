c4b49acbda4640940c9ff54c8292a89201ce6b41
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-12-19 16:48:43 -0800

    Time: 33 ms (29.35%), Space: 42.9 MB (35.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,21 +1,26 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        
-        if (s.Length != t.Length)
-            return false;
-        
-        char[] sStr = s.ToArray();
-        char[] tStr = t.ToArray();
-        
-        Array.Sort(sStr);
-        Array.Sort(tStr);
-
-        for (int i = 0; i < sStr.Length; i++)
-        {
-            if (sStr[i] != tStr[i])
+            if (s.Length != t.Length)
                 return false;
-        }
+            
+            Dictionary<char, int> sList = new Dictionary<char, int>();
 
-        return true;
+            for (int i = 0; i < s.Length; i++)
+                if (sList.ContainsKey(s[i]))
+                    sList[s[i]] += 1;
+                else
+                    sList.Add(s[i], 1);
+            
+            for (int i = 0; i < t.Length; i++)
+                if (!sList.ContainsKey(t[i]))
+                    return false;
+                else
+                    sList[t[i]] -= 1;
+
+            foreach (var val in sList.Values)
+                if (val != 0)
+                    return false;
+            
+            return true;
     }
 }
