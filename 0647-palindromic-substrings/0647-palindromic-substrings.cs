ee0dcf30d0c541038dd2a737413b568bca161911
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-12-21 21:39:53 -0800

    Time: 56 ms (16.63%), Space: 54.4 MB (6.79%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cs
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cs
@@ -1,31 +1,27 @@
 public class Solution {
     public int CountSubstrings(string s)
     {
-        List<string> count = new List<string>();
-
+        int count = 0;
         for (int i = 0; i < s.Length; i++)
         {
             int l = i;
             int r = i;
-            GetPalindrome(s, l, r, count);
-            GetPalindrome(s, l, r + 1, count);
+            count += GetPalindrome(s, l, r) + GetPalindrome(s, l, r + 1);
         }
 
-        return count.Count;
+        return count;
     }
 
-    private void GetPalindrome(string s, int l, int r, List<string> count)
+    private int GetPalindrome(string s, int l, int r)
     {
-        if (!(l >= 0 && r < s.Length))
-            return;
-        if (!s.Substring(l, 1).Equals(s.Substring(r, 1)))
-            return;
-
+        int count = 0;
         while (l >= 0 && r < s.Length && s.Substring(l, 1).Equals(s.Substring(r, 1)))
         {
-            count.Add(s.Substring(l, r - l + 1));
             l--;
             r++;
+            count++;
         }
+
+        return count;
     }
 }
