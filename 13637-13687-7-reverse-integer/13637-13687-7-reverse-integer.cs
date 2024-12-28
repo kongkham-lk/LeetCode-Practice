9fd72f530672bf53c9496d3b7f530c4d703840c1
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2024-12-28 15:36:38 -0800

    Time: 30 ms, Memory: 27.9 MB - LeetHub

--- 13637-13687-7-reverse-integer/13637-13687-7-reverse-integer.cs
+++ 13637-13687-7-reverse-integer/13637-13687-7-reverse-integer.cs
@@ -1,24 +1,15 @@
 public class Solution {
     public int Reverse(int x) {
-        if (x == 0)
-            return x;
-        bool isNeg = x < 0;
-        if (isNeg)
-            x *= -1;
-
-        int result = 0;
-        long temp = result;
-        while (x > 0)
-        {
-            temp *= 10;
-            temp += x % 10;
-            x = x / 10;
-            result = (int)temp;
-            if (temp != result)
-                return 0;
-        }
-
-        if (isNeg) return result * -1;
-        return result;
+            int result = 0;
+            int temp = 0;
+            while (x != 0)
+            {
+                if (result > Int32.MaxValue / 10) return 0;
+                if (result < Int32.MinValue / 10) return 0;
+                temp = x % 10;
+                result = result * 10 + temp;
+                x /= 10;
+            }
+            return result;
     }
 }
