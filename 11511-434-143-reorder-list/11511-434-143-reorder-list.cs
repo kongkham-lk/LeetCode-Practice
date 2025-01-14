eed819d11f68267f5706dd538aaf2a00b8232b96
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-01-13 19:25:07 -0800

    Time: 0 ms, Memory: 49.6 MB - LeetHub

--- 11511-434-143-reorder-list/11511-434-143-reorder-list.cs
+++ 11511-434-143-reorder-list/11511-434-143-reorder-list.cs
@@ -10,34 +10,83 @@
  * }
  */
 public class Solution {
+        // =================== implement recursion ============================//
+
+        // public void ReorderList(ListNode root)
+        // {
+        //     if (root == null)
+        //         return;
+        //     ListNode curr = root;
+        //     ListNode head = root;
+        //     ReorderList(ref head, ref curr);
+        // }
+    
+        // public void ReorderList(ref ListNode head, ref ListNode curr) 
+        // {
+        //     if (curr == null) return;
+            
+        //     // use recursive to reversely iterate linked list
+        //     ReorderList(ref head, ref curr.next);
+            
+        //     ListNode tail = curr; // fetch the new tail node
+        //     if (head == tail.next) // if tail point back to head node, remove the last node's next pointer
+        //         head.next = null;
+        //     if (head.next != null)
+        //     {
+        //         ListNode temp = head;
+        //         head = head.next;
+        //         temp.next = tail;
+        //         temp = temp.next;
+        //         temp.next = head;
+        //         if (head == tail) // if head point to tail node, remove the last node's next pointer
+        //             head.next = null;
+        //     }
+        // }
+
+        // =================== implement fast slow pointer (more efficient) ==========================//
+
         public void ReorderList(ListNode root)
         {
-            if (root == null)
+            if (root == null || root.next == null || root.next.next == null)
                 return;
-            ListNode curr = root;
+
+            ListNode s = root;
+            ListNode f = root.next;
+
+            // get mid node of linked list
+            while (f.next != null && f.next.next != null)
+            {
+                s = s.next;
+                f = f.next.next;
+            }
+
+            // swap pointer
+            ListNode prev = s;
+            s = s.next;
+
+            while (s != null)
+            {
+                ListNode temp = s;
+                s = s.next;
+                temp.next = prev;
+                prev = temp;
+            }
+
+            // Merge head and tail node
             ListNode head = root;
-            ReorderList(ref head, ref curr);
-        }
-    
-        public void ReorderList(ref ListNode head, ref ListNode curr) 
-        {
-            if (curr == null) return;
-            
-            // use recursive to reversely iterate linked list
-            ReorderList(ref head, ref curr.next);
-            
-            ListNode tail = curr; // fetch the new tail node
-            if (head == tail.next) // if tail point back to head node, remove the last node's next pointer
-                head.next = null;
-            if (head.next != null)
+            while (head != prev || head != prev.next)
             {
                 ListNode temp = head;
                 head = head.next;
-                temp.next = tail;
+                temp.next = prev;
+                prev = prev.next;
                 temp = temp.next;
                 temp.next = head;
-                if (head == tail) // if head point to tail node, remove the last node's next pointer
-                    head.next = null;
+                if (head == prev.next)
+                {
+                    prev.next = null;
+                    break;
+                }
             }
         }
 }
