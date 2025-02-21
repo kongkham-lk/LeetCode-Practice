118a5993ba7a7b4b1b38b3bc7963f5cedbebbe6c
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-02-21 02:51:47 -0800

    Time: 14 ms, Memory: 64.3 MB - LeetHub

---
+++ 23717-1516-238-product-of-array-except-self/23717-1516-238-product-of-array-except-self.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int[] results = new int[nums.Length];
+        int pre = 1; // initilized the left
+        int pos = 1; // initilized the right
+        
+        for (int i = 0; i < nums.Length; i++) {
+            results[i] = pre; // assign all left to an empty result list
+            pre = results[i] * nums[i]; // multiply the left with current nums to be the nex element's left
+        }
+        
+        // skip last element
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            results[i] *= pos; // multiply the right and the left
+            pos *= nums[i]; // multiply the right with current to be nex element's right
+        }
+        return results;
+    }
+}
+// 24 12 8 6
+// 24
