a14388da6487bdb9d246e0e2553f397620f545ba
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-04 01:31:07 -0700

    Time: 3 ms (85.71%), Space: 60.7 MB (28.57%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cs
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cs
@@ -1,31 +1,58 @@
 public class Solution {
+    // Solution 2 - Optimized
     public int MinDominoRotations(int[] tops, int[] bottoms) {
-        int[] res = new int[6+1];
-        int[] ftNum = new int[6];
-        int[] fbNum = new int[6];
-            
-        for (int i = 0; i < tops.Length; i++) {
-            if (tops[i] == bottoms[i])
-                res[tops[i]]++;
-            else {
-                res[tops[i]]++;
-                res[bottoms[i]]++;
-            }
-            ftNum[tops[i]-1]++;
-            fbNum[bottoms[i]-1]++;
-        }
-        
-        int maxFace = -1;
-        int targetFace = -1;
+        int res = int.MaxValue;
         for (int i = 1; i <= 6; i++) {
-            if (maxFace < res[i]) {
-                maxFace = res[i];
-                targetFace = i - 1;
+            int temp = GetRotation(tops, bottoms, i);
+            if (temp != int.MaxValue) {
+                Console.Write(i + ", ");
+                res = Math.Min(res, temp);
             }
         }
-        if (maxFace == tops.Length)
-            return tops.Length - Math.Max(ftNum[targetFace], fbNum[targetFace]);
-            
-        return -1;
+        return res == int.MaxValue ? -1 : res;
     }
+
+    private int GetRotation(int[] tops, int[] bottoms, int target) {
+        int t = 0, b = 0;
+
+        for (int i = 0; i < tops.Length; i++) {
+            if (tops[i] != target && bottoms[i] != target)
+                return int.MaxValue;
+
+            if (tops[i] != target) t++;
+            else if (bottoms[i] != target) b++;
+        }
+        return Math.Min(t, b);
+    }
+
+    // // Solution 1 - Initial
+    // public int MinDominoRotations(int[] tops, int[] bottoms) {
+    //     int[] res = new int[6+1];
+    //     int[] ftNum = new int[6];
+    //     int[] fbNum = new int[6];
+            
+    //     for (int i = 0; i < tops.Length; i++) {
+    //         if (tops[i] == bottoms[i])
+    //             res[tops[i]]++;
+    //         else {
+    //             res[tops[i]]++;
+    //             res[bottoms[i]]++;
+    //         }
+    //         ftNum[tops[i]-1]++;
+    //         fbNum[bottoms[i]-1]++;
+    //     }
+        
+    //     int maxFace = -1;
+    //     int targetFace = -1;
+    //     for (int i = 1; i <= 6; i++) {
+    //         if (maxFace < res[i]) {
+    //             maxFace = res[i];
+    //             targetFace = i - 1;
+    //         }
+    //     }
+    //     if (maxFace == tops.Length)
+    //         return tops.Length - Math.Max(ftNum[targetFace], fbNum[targetFace]);
+            
+    //     return -1;
+    // }
 }
