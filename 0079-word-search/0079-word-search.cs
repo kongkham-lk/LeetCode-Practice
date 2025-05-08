b8adfe1eb5c7f432061918a71c954e1b87709aaa
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-08 00:14:01 -0700

    Time: 2008 ms (10.34%), Space: 43.6 MB (48.28%) - LeetHub

--- 0079-word-search/0079-word-search.cs
+++ 0079-word-search/0079-word-search.cs
@@ -1,8 +1,11 @@
 public class Solution {
+    int rSize, cSize;
     HashSet<(int, int)> dp = new HashSet<(int, int)>();
     public bool Exist(char[][] board, string word) {
-        for (int i = 0; i < board.Length; i++) {
-            for (int j = 0; j < board[i].Length; j++) {
+        rSize = board.Length;
+        cSize = board[0].Length;
+        for (int i = 0; i < rSize; i++) {
+            for (int j = 0; j < cSize; j++) {
                 if (FindWord(board, i, j, word, 0))
                     return true;
             }
@@ -13,17 +16,17 @@ public class Solution {
     private bool FindWord(char[][] board, int i, int j, string word, int c) {
         if (c == word.Length)
             return true;
-        else if (i < 0 || j < 0 || i >= board.Length || j >= board[i].Length 
+        else if (i < 0 || j < 0 || i >= rSize || j >= cSize 
             || dp.Contains((i, j)) || !board[i][j].Equals(word[c])) 
             return false;
         
-        dp.Add((i, j));
+        dp.Add((i, j)); // memo when there is match char
         c++;
         bool res = FindWord(board, i, j-1, word, c) 
             || FindWord(board, i, j+1, word, c) 
             || FindWord(board, i-1, j, word, c) 
             || FindWord(board, i+1, j, word, c);
-        dp.Remove((i, j));
+        dp.Remove((i, j)); // remove the last add match and looking for a new match on different direction
 
         return res;
     }
