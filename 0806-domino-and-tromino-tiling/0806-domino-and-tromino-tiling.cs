40b4fa7e074daa6e195c2bfd48ca6d07d2b17815
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-07 17:56:54 -0700

    Time: 0 ms (100%), Space: 29.2 MB (43.97%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cs
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cs
@@ -1,24 +1,24 @@
 public class Solution {
+    const int MOD = (int)1e9 + 7;
+    int[] dp;
+
     public int NumTilings(int n) {
-        double[] dp = new double[n+1];
-        return SaveDP(n, dp);
+        dp = new int[n+1];
+        return SaveDP(n);
     }
 
-    private int SaveDP(int n, double[] dp) {
+    private int SaveDP(int n) {
         if (n <= 1) return 1;
         else if (n == 2) return 2;
         else if (n == 3) return 5;
-        for (int i = 0; i <= 3; i++) {
-            dp[i] = SaveDP(i, dp);
-            Console.WriteLine(i + ": " + dp[i]);
-        }
-        int MOD = 1000000007;
-        for (int i = 4; i <= n; i++) {
-            dp[i] = (2 * dp[i-1] + dp[i-3]) % MOD;
-            Console.WriteLine(i + ": " + dp[i]);
-        }
+        
+        for (int i = 0; i <= 3; i++)
+            dp[i] = SaveDP(i);
             
-        return (int) dp[n];
+        for (int i = 4; i <= n; i++)
+            dp[i] = ((2 * dp[i-1]) % MOD + dp[i-3]) % MOD;
+            
+        return dp[n];
     }
 
 }
