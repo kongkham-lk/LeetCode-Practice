a478f9d983bfe4fa4df70936585c91ccaa71a155
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-08 02:04:16 -0700

    Time: 71 ms (23.39%), Space: 41.4 MB (12.17%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
@@ -1,17 +1,15 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        int i = 0;
         string res = "";
         int w1Size = word1.Length;
         int w2Size = word2.Length;
-        while (i < w1Size || i < w2Size) {
+        int size = Math.Max(w1Size, w2Size);
+        for (int i = 0; i < size; i++) {
             if (i < w1Size)
                 res += word1[i];
             if (i < w2Size)
                 res += word2[i];
-            i++;
         }
-
         return res;
     }
 }
