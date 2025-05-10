cdff87f0c7a9885e517ccdd53b1f7eaa7bb5efdf
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-09 18:21:46 -0700

    Time: 7 ms (73.65%), Space: 18.2 MB (54.63%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -6,22 +6,20 @@ class Solution:
 
             plot = 0
             empty = True
-
-            if (flowerbed[0] == 0 
-                and flowerbed[1] == 0):
+            tempPlot = flowerbed.copy()
+            if (tempPlot[0] == 0 
+                and tempPlot[1] == 0):
                 plot += 1
-                flowerbed[0] = 1
-            for i in range(0,len(flowerbed)-1):
-                if (flowerbed[i-1] == 0 
-                    and flowerbed[i] == 0 
-                    and flowerbed[i+1] == 0):
+                tempPlot[0] = 1
+            for i in range(0,len(tempPlot)-1):
+                if (tempPlot[i-1] == 0 
+                    and tempPlot[i] == 0 
+                    and tempPlot[i+1] == 0):
                     plot += 1
-                    flowerbed[i] = 1
-            if (flowerbed[len(flowerbed)-2] == 0 
-                and flowerbed[len(flowerbed)-1] == 0):
+                    tempPlot[i] = 1
+            if (tempPlot[len(tempPlot)-2] == 0 
+                and tempPlot[len(tempPlot)-1] == 0):
                 plot += 1
             return plot
-        
-        foundPlot = countEmptyPlot(flowerbed)
-        print("found plantable: ", foundPlot)
-        return foundPlot >= n
+            
+        return countEmptyPlot(flowerbed) >= n
