c57ec175adbc711e93c318b99458145e40cdca1e
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-14 16:41:33 -0700

    Time: 11 ms (96.11%), Space: 37.7 MB (24.14%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,5 +1,4 @@
 class Solution:
-    __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
     def increasingTriplet(self, nums: List[int]) -> bool:
         first = second = float('inf')
         for n in nums:
