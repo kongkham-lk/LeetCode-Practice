14e9009efbe8b677eebf37d4accc665e179c4fd7
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-13 21:40:57 -0700

    Time: 39 ms (17.49%), Space: 27 MB (19.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,8 +4,8 @@ class Solution:
         pre = [0] * n
         post = [0] * n
         res = [0] * n
+        
         pre[0] = post[n-1] = 1
-
         for i in range(1, n):
             pre[i] = pre[i-1] * nums[i-1]
         for i in range(n-2, -1, -1):
