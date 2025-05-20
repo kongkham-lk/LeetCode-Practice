c3a5effe1dced7d2b10c8931bb81b8c368312c26
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-19 22:44:55 -0700

    Time: 127 ms (80.56%), Space: 52.4 MB (75.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,18 +1,21 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-        foreach(var r in grid) {
-            Console.Write("[ ");
-            foreach (var c in r)
-                Console.Write(c + " ");
-            Console.Write("]\n");
-        }
+        if (grid.Length < 0) return 0;
+        
+        // // For debugging
+        // foreach(var r in grid) {
+        //     Console.Write("[ ");
+        //     foreach (var c in r)
+        //         Console.Write(c + " ");
+        //     Console.Write("]\n");
+        // }
 
         int res = 0;
         for (int i = 0; i < grid.Length; i++) {
             for (int j = 0; j < grid[0].Length; j++) {
                 if (grid[i][j].Equals('1')) {
                     res++;
-                    this.ExploreLand(grid, i, j);
+                    ExploreLand(grid, i, j);
                 }
             }
         }
@@ -24,9 +27,9 @@ public class Solution {
         
         grid[i][j] = '0';
 
-        this.ExploreLand(grid, i-1, j);
-        this.ExploreLand(grid, i+1, j);
-        this.ExploreLand(grid, i, j-1);
-        this.ExploreLand(grid, i, j+1);
+        ExploreLand(grid, i-1, j);
+        ExploreLand(grid, i+1, j);
+        ExploreLand(grid, i, j-1);
+        ExploreLand(grid, i, j+1);
     }
 }
