2c0ded255e206c3eca7cedee6c0f6d0a9c240ee1
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-19 22:42:46 -0700

    Time: 175 ms (6.72%), Space: 58 MB (11.63%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int NumIslands(char[][] grid) {
+        foreach(var r in grid) {
+            Console.Write("[ ");
+            foreach (var c in r)
+                Console.Write(c + " ");
+            Console.Write("]\n");
+        }
+
+        int res = 0;
+        for (int i = 0; i < grid.Length; i++) {
+            for (int j = 0; j < grid[0].Length; j++) {
+                if (grid[i][j].Equals('1')) {
+                    res++;
+                    ExploreLand(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void ExploreLand(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || !grid[i][j].Equals('1')) return;
+        
+        grid[i][j] = '0';
+
+        ExploreLand(grid, i-1, j);
+        ExploreLand(grid, i+1, j);
+        ExploreLand(grid, i, j-1);
+        ExploreLand(grid, i, j+1);
+    }
+}
