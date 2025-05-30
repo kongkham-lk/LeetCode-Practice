fcec5b81a8b792d9032d6180cbf6c5bc2ffe6ab8
Kongkham LUANGKHOT <kongkham.luangkhot@gmail.com>
2025-05-29 18:59:42 -0700

    Time: 3 ms (84.59%), Space: 60.6 MB (21.98%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cs
@@ -2,26 +2,15 @@ public class Solution {
     public double FindMaxAverage(int[] nums, int k) {
         if (k == 0) return 0;
         else if (nums.Length == 1) return nums[0];
-
-        int l = 0;
-        int r = 1;
-        int max = nums[l];
-        // Console.WriteLine(nums[l] + ": " + max);
-
-        while (r < k) {
-            max += nums[r];
-            // Console.WriteLine(nums[r] + ": " + max);
-            r++;
-        }
+        
+        int max = nums[0];
+        for (int i = 1; i < k; i++)
+            max += nums[i];
 
         int curr = max;
-        while (r < nums.Length) {
-            // Console.Write(curr + " - " + nums[l] + " + " + nums[r] + " => ");
-            curr = curr + nums[r] - nums[l];
+        for (int i = k; i < nums.Length; i++) {
+            curr = curr + nums[i] - nums[i-k];
             max = Math.Max(max, curr);
-            l++;
-            r++;
-            // Console.WriteLine(curr + ", " + max);
         }
         return (double)max / k;
     }
